c0f558629c7965ddd29f53551a605fcfaf795990
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2025-05-14 14:36:24 +0300

    Update restZeebeClient to auto. update token

--- src/main/java/camunda/client/RestZeebeClient.java
+++ src/main/java/camunda/client/RestZeebeClient.java
@@ -7,28 +7,58 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.time.Instant;
 import java.util.Map;
 
 @Component
 public class RestZeebeClient {
+    @Value("${CAMUNDA_CLIENT_AUTH_CLIENT_ID}")
+    private String clientId;
+    @Value("${CAMUNDA_CLIENT_AUTH_CLIENT_SECRET}")
+    private String clientSecret;
     @Value("${CAMUNDA_CLIENT_ZEEBE_REST_ADDRESS}")
     private String zeebeRestAddress;
-
-    @Value("${ZEEBE_TOKEN}")
     private String zeebeToken;
-
-    private RestClient restClient;
+    private Instant zeebeTokenExpiryTime;
+    private static final String ZEEBE_AUTHORIZATION_SERVER_URL = "https://login.cloud.camunda.io/oauth/token";
+    private static final String ZEEBE_TOKEN_AUDIENCE = "zeebe.camunda.io";
+    private RestClient authClient;
+    private RestClient zeebeClient;
 
     @PostConstruct
     public void buildRestClient() {
-        restClient = RestClient.builder()
+        authClient = RestClient.builder()
+                .baseUrl(ZEEBE_AUTHORIZATION_SERVER_URL)
+                .build();
+        zeebeClient = RestClient.builder()
                 .baseUrl(zeebeRestAddress)
-                .defaultHeader("Authorization", "Bearer " + zeebeToken)
                 .build();
     }
 
+    public void updateZeebeToken() {
+        Map<String, Object> tokenResponse = authClient
+                .post()
+                .body(Map.of(
+                        "client_id", clientId,
+                        "client_secret", clientSecret,
+                        "audience", ZEEBE_TOKEN_AUDIENCE))
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        zeebeToken = (String) tokenResponse.get("access_token");
+        zeebeTokenExpiryTime = Instant.now().plusSeconds((Integer) tokenResponse.get("expires_in"));
+    }
+
+    public void checkAndUpdateToken() {
+        if (zeebeToken == null || Instant.now().isAfter(zeebeTokenExpiryTime)) {
+            updateZeebeToken();
+        }
+    }
+
     public void startProcessInstance(Map<String, Object> requestBody) {
-        restClient.post()
+        checkAndUpdateToken();
+        zeebeClient.post()
                 .uri("/v2/process-instances")
                 .body(requestBody)
                 .retrieve()
@@ -37,7 +67,8 @@ public class RestZeebeClient {
     }
 
     public ActivatedJobs activateJobs(Map<String, Object> requestBody) {
-        return restClient.post()
+        checkAndUpdateToken();
+        return zeebeClient.post()
                 .uri("/v2/jobs/activation")
                 .body(requestBody)
                 .retrieve()
@@ -45,7 +76,8 @@ public class RestZeebeClient {
     }
 
     public void completeJob(Long jobKey, Map<String, Object> requestBody) {
-        restClient.post()
+        checkAndUpdateToken();
+        zeebeClient.post()
                 .uri("/v2/jobs/{jobKey}/completion", jobKey)
                 .body(requestBody)
                 .retrieve()
@@ -54,7 +86,8 @@ public class RestZeebeClient {
     }
 
     public void publishMessage(Map<String, Object> requestBody) {
-        restClient.post()
+        checkAndUpdateToken();
+        zeebeClient.post()
                 .uri("/v2/messages/publication")
                 .body(requestBody)
                 .retrieve()
@@ -63,7 +96,8 @@ public class RestZeebeClient {
     }
 
     public void completeUserTask(Long userTaskKey, Map<String, Object> requestBody) {
-        restClient.post()
+        checkAndUpdateToken();
+        zeebeClient.post()
                 .uri("/v2/user-tasks/{userTaskKey}/completion", userTaskKey)
                 .body(requestBody)
                 .retrieve()
